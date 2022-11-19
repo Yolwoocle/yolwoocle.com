6d4e2376540e67bcdf16c28148856f8b88d8184d
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2022-11-19 23:02:06 +0100

    New website

---
+++ new_website/js/art.js
@@ -0,0 +1,15 @@
+// var css = '.card:hover{ transform: rotate(${Math.random()*6 - 3}deg) }';
+// var style = document.createElement('style');
+
+// if (style.styleSheet) {
+//     style.styleSheet.cssText = css;
+// } else {
+//     style.appendChild(document.createTextNode(css));
+// }
+
+// document.getElementsByTagName('head')[0].appendChild(style);
+
+// const cards = document.getElementsByClassName("card");
+// for (let i=0; i < cards.length; i++) {
+//     console.log(i)
+// }
